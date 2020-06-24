562e736562bc9cb201edd0557c4c44ba0e704823
Jerome Wu <jeromewus@gmail.com>
2020-06-24 13:40:56 +0800

    Add combined keys

--- ir-remote/ir-remote.ino
+++ ir-remote/ir-remote.ino
@@ -1,15 +1,22 @@
 #include <IRremote.h>
 
-#define IR_RIGHT    0x21035431
-#define IR_LEFT     0x983AB4C1
-#define IR_UP       0xC20370A1
-#define IR_DOWN     0x81930A09
-#define IR_ENTER    0xBB0ED9E1
-#define IR_PLAY     0x68E839F1  //Space  play
-#define IR_STOP     0xE01F9A81  //x      0
-#define IR_ESC      0xE8EDC338  //Esc
-#define IR_VOL_DOWN 0x240C9161
-#define IR_VOL_UP   0xA26409C9
+#define IR_RIGHT    0x807F827D
+#define IR_LEFT     0x807F42BD
+#define IR_UP       0x807FE817
+#define IR_DOWN     0x807F58A7
+#define IR_ENTER    0x807FA857
+#define IR_PLAY     0x807F08F7  //Space  play
+#define IR_STOP     0x807FEA15  //x      0
+#define IR_ESC      0x807F6897  //Esc
+/*
+ *#define IR_VOL_DOWN 0x240C9161
+ *#define IR_VOL_UP   0xA26409C9
+ */
+
+#define IR_INTERNAL_MONITOR_ONLY   0x807F22DD
+#define IR_EXTERNAL_MONITOR_ONLY   0x807FA25D
+#define IR_START_KODI              0x807FE21D
+#define IR_OUTPUT_AUDIO            0x807F629D
 
 #define KEY_UP_ARROW       82
 #define KEY_DOWN_ARROW     81
@@ -21,8 +28,31 @@
 #define KEY_MINUS          45
 #define KEY_PLUS           46
 #define KEY_X              27
+#define KEY_1              30
+#define KEY_2              31 
+#define KEY_K              14
+#define KEY_A              4
+
+#define LEFT_CTRL   0
+#define LEFT_SHIFT  1
+#define LEFT_ALT    2
+#define LEFT_GUI    3
+#define RIGHT_CTRL  4
+#define RIGHT_SHIFT 5
+#define RIGHT_ALT   6
+#define RIGHT_GUI   7
 
 const int RECV_PIN = 7;
+const uint8_t MODIFIER_MASKS[8] = {
+  B00000001,
+  B00000010,
+  B00000100,
+  B00001000,
+  B00010000,
+  B00100000,
+  B01000000,
+  B10000000
+};
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 uint8_t buf[8] = { 0 };
@@ -35,39 +65,54 @@ void setup(){
 
 void loop(){
   if (irrecv.decode(&results)){
+    int mods[] = {LEFT_ALT, LEFT_CTRL};
     unsigned long code = results.value;
     //Serial.println(code, HEX);
     switch(code) {
       case IR_UP:
-        sendKey(KEY_UP_ARROW);
+        sendKey(KEY_UP_ARROW, 0, 0);
         break;
       case IR_DOWN:
-        sendKey(KEY_DOWN_ARROW);
+        sendKey(KEY_DOWN_ARROW, 0, 0);
         break;
       case IR_LEFT:
-        sendKey(KEY_LEFT_ARROW);
+        sendKey(KEY_LEFT_ARROW, 0, 0);
         break;
       case IR_RIGHT:
-        sendKey(KEY_RIGHT_ARROW);
+        sendKey(KEY_RIGHT_ARROW, 0, 0);
         break;
       case IR_ENTER:
-        sendKey(KEY_RETURN);
+        sendKey(KEY_RETURN, 0, 0);
         break;
       case IR_PLAY:
-        sendKey(KEY_SPACE);
+        sendKey(KEY_SPACE, 0, 0);
         break;
       case IR_STOP:
-        sendKey(KEY_X);
+        sendKey(KEY_X, 0, 0);
         break;
       case IR_ESC:
-        sendKey(KEY_ESC);
+        sendKey(KEY_ESC, 0, 0);
         break;
-      case IR_VOL_DOWN:
-        sendKey(KEY_MINUS);
+      case IR_INTERNAL_MONITOR_ONLY:
+        sendKey(KEY_1, mods, 2);
         break;
-      case IR_VOL_UP:
-        sendKey(KEY_PLUS);
+      case IR_EXTERNAL_MONITOR_ONLY:
+        sendKey(KEY_2, mods, 2);
         break;
+      case IR_START_KODI:
+        sendKey(KEY_K, mods, 2);
+        break;
+      case IR_OUTPUT_AUDIO:
+        sendKey(KEY_A, mods, 2);
+        break;
+      /*
+       *case IR_VOL_DOWN:
+       *  sendKey(KEY_MINUS);
+       *  break;
+       *case IR_VOL_UP:
+       *  sendKey(KEY_PLUS);
+       *  break;
+       */
     }
     irrecv.resume();
   }
@@ -80,7 +125,10 @@ void releaseKey()
   Serial.write(buf, 8);  // Release key  
 }
 
-void sendKey(int keyNum){
+void sendKey(int keyNum, int *modifiers, int modifiersLen) {
+  for (int i = 0; i < modifiersLen; i++) {
+    buf[0] = buf[0] | MODIFIER_MASKS[modifiers[i]];
+  }
   buf[2] = keyNum;
   Serial.write(buf, 8);
   releaseKey();
